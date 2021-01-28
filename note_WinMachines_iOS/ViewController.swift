bd7ae61621f7a0af1f25a8582032131030b297eb
Tej <codewithtej@gmail.com>
2021-01-28 07:47:11 -0500

    Initial Commit

---
+++ note_WinMachines_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  note_WinMachines_iOS
+//
+//  Created by Tej on 28/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
