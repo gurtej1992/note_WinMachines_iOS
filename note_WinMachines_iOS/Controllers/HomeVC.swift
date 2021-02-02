09342092b2e82024099165f9235b666e66ab6acf
Sukhvir <sandhusukhvir20@gmail.com>
2021-02-02 11:06:10 -0500

    Relation added in CoreData

--- note_WinMachines_iOS/Controllers/HomeVC.swift
+++ note_WinMachines_iOS/Controllers/HomeVC.swift
@@ -16,3 +16,15 @@ class HomeVC: UIViewController {
     @IBAction func handleAdd(_ sender: UIBarButtonItem) {
     }
 }
+extension HomeVC : UICollectionViewDelegate , UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
+
