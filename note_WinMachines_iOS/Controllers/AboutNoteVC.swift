edfce1cfe8e62a090c93500d9e8ac9ebfc577f76
Tej <codewtihtej@gmail.com>
2021-02-04 12:52:30 -0500

    More Info Done..

--- note_WinMachines_iOS/Controllers/AboutNoteVC.swift
+++ note_WinMachines_iOS/Controllers/AboutNoteVC.swift
@@ -10,11 +10,11 @@ import MapKit
 class AboutNoteVC: UITableViewController {
     var selectedNote : Notes?
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var lblLocation: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(selectedNote)
         if let note =  selectedNote{
             let coordinates = CLLocationCoordinate2D(latitude: note.lat , longitude: note.long)
             let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
@@ -23,6 +23,21 @@ class AboutNoteVC: UITableViewController {
             let pin = MKPointAnnotation()
             pin.coordinate = coordinates
             mapView.addAnnotation(pin)
+            
+            let geo = CLGeocoder()
+            
+            geo.reverseGeocodeLocation(CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)) { (placeMark, err) in
+                if placeMark?.count != nil{
+                    if let placeMark = placeMark?.first{
+                        let Marker = MKPointAnnotation()
+                        Marker.coordinate = coordinates
+                        Marker.title = placeMark.name
+                        self.lblLocation.text = "\(placeMark.name ?? "")"
+                        self.mapView.addAnnotation(Marker)
+                        
+                    }
+                }
+        }
         }
     }
     @IBAction func handleBack(_ sender: UIButton) {
@@ -34,6 +49,14 @@ class AboutNoteVC: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return 2
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return (tableView.frame.height * 80) / 100
+        }
+        else{
+            return tableView.estimatedRowHeight
+        }
     }
 }
